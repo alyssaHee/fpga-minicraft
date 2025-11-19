0bb84bd345eeeb186d84ea98f2b28eb54ac0b958
jmswws75 <62361325+jmswws75@users.noreply.github.com>
2025-11-18 21:03:22 -0500

    Add files via upload

---
+++ object_map_WRITE.v
@@ -0,0 +1,28 @@
+// inputs a 1 or 0 into the map if a block is placed or broken (to be called by placing/breaking logic)
+module object_map_WRITE (Resetn, Clock, go, screenX, screenY, data_in, map);
+	input Resetn, Clock, go;
+	input [9:0] screenX;		// original coordinate from vga
+	input [8:0] screenY;
+	input data_in;
+
+	output reg [1199:0] map;
+	
+	wire [6:0] blockX = screenX / 16;		// LOCATIONS IN CONDENSED FORM (0-39, 0,29)
+	wire [5:0] blockY = screenY / 16;
+	wire [10:0] blockAddr = blockY*40+blockX;		// Get the address of the location to write to
+	
+	always @(posedge Clock)
+	begin
+		if(!Resetn)
+		begin   // initialize the map here
+			map = 1200'b0;
+			map[2] = 1'b1;
+			map[42] = 1'b1;
+			map[77] = 1'b1;
+			
+		end
+		// if the write enable is 1, write to the map
+		else if (go)
+			map[blockAddr] <= data_in;
+	end
+endmodule
