7f4d6e21ce6df4b50fb558f67abe8fac3670fa09
jmswws75 <62361325+jmswws75@users.noreply.github.com>
2025-11-25 00:35:17 -0500

    implemented initialization

--- object_map_WRITE_1.v
+++ object_map_WRITE_1.v
@@ -9,9 +9,39 @@ module object_map_WRITE_1 (Resetn, Clock, go, screenX, screenY, data_in, map);
 	initial
 	begin
 		map = 1200'b0;
-		map[2] = 1'b1;
-		map[42] = 1'b1;
-		map[77] = 1'b1;
+		map[1 * 40 + 14] = 1'b1;
+		map[2 * 40 + 14] = 1'b1;
+		map[3 * 40 + 14] = 1'b1;
+		map[4 * 40 + 14] = 1'b1;
+		map[5 * 40 + 14] = 1'b1;
+		map[6 * 40 + 14] = 1'b1;
+		map[7 * 40 + 14] = 1'b1;
+		map[8 * 40 + 14] = 1'b1;
+		map[9 * 40 + 14] = 1'b1;
+		
+		map[9 * 40 + 15] = 1'b1;
+		map[9 * 40 + 16] = 1'b1;
+		map[9 * 40 + 17] = 1'b1;
+		map[9 * 40 + 18] = 1'b1;
+		map[9 * 40 + 19] = 1'b1;
+		map[9 * 40 + 20] = 1'b1;
+		map[9 * 40 + 21] = 1'b1;
+		
+		map[1 * 40 + 22] = 1'b1;
+		map[2 * 40 + 22] = 1'b1;
+		map[3 * 40 + 22] = 1'b1;
+		map[6 * 40 + 22] = 1'b1;
+		map[7 * 40 + 22] = 1'b1;
+		map[8 * 40 + 22] = 1'b1;
+		map[9 * 40 + 22] = 1'b1;
+		
+		map[1 * 40 + 15] = 1'b1;
+		map[1 * 40 + 16] = 1'b1;
+		map[1 * 40 + 17] = 1'b1;
+		map[1 * 40 + 18] = 1'b1;
+		map[1 * 40 + 19] = 1'b1;
+		map[1 * 40 + 20] = 1'b1;
+		map[1 * 40 + 21] = 1'b1;
 	end
 	
 	wire [6:0] blockX = screenX / 16;		// LOCATIONS IN CONDENSED FORM (0-39, 0,29)
